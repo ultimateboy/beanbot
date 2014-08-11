90a2b3806d94b9227561498670c8c0fc8be9b46f
Erin Corson <erin.corson@colorado.edu>
2014-08-10 19:52:48 -0600

    trying redis

--- beanbot/celeryconfig.py
+++ beanbot/celeryconfig.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 
-BROKER_URL = 'mongodb://localhost:27017/beanbot'
+BROKER_URL = 'redis://localhost:6379/0'
+#BROKER_URL = 'mongodb://localhost:27017/beanbot'
 
 CELERY_TIMEZONE = 'UTC'
 
