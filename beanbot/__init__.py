04c79d2843854ad13593a51ede35203033600b1f
Erin Corson <erin.corson@colorado.edu>
2014-08-10 18:40:50 -0600

    moving chat commands out to chat.py

--- beanbot/__init__.py
+++ beanbot/__init__.py
@@ -1,22 +1,11 @@
 import usb.core
 import usb.util
-import xmpp
+from beanbot.chat import send_jabber_message
 
 try:
     from local_settings import *
 except ImportError:
     raise Exception("You need a local_settings.py file!")
-
-def send_jabber_message(messagebody):
-    client = xmpp.Client(JABBER_SERVER)
-    client.connect(server=(JABBER_SERVER, JABBER_PORT))
-    client.auth(JABBER_USER,JABBER_PASS,JABBER_NAME)
-    client.sendInitPresence()
-    client.send(xmpp.Presence(to="%s/%s" % (JABBER_ROOM, JABBER_NAME)))
-    message = xmpp.protocol.Message(body=messagebody)
-    message.setTo(JABBER_ROOM)
-    message.setType('groupchat')
-    client.send(message)
 
 def main():
     # find the USB device
