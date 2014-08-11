dd8b01c783763c9c76fe8ca3e31a7074e9610927
Erin Corson <erin.corson@colorado.edu>
2014-08-10 18:46:34 -0600

    this function has moved

--- beanbot/__init__.py
+++ beanbot/__init__.py
@@ -88,16 +88,6 @@ def series_to_animated_gif(L, filepath):
     imgs.close()
     print('saved animated.gif')
 
-def send_jabber_message(messagebody):
-    client = xmpp.Client(JABBER_SERVER)
-    client.connect(server=(JABBER_SERVER, JABBER_PORT))
-    client.auth(JABBER_USER,JABBER_PASS,JABBER_NAME)
-    client.sendInitPresence()
-    client.send(xmpp.Presence(to="%s/%s" % (JABBER_ROOM, JABBER_NAME)))
-    message = xmpp.protocol.Message(body=messagebody)
-    message.setTo(JABBER_ROOM)
-    message.setType('groupchat')
-    client.send(message)
 
 def main():
 
