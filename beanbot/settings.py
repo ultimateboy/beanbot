9c1da967dabc76eadbf0c867127b9f53652b6645
Matt Tucker <ultimateboy@gmail.com>
2014-08-30 18:03:49 +0000

    Moving scale settings out of local config.

--- beanbot/settings.py
+++ beanbot/settings.py
@@ -1,6 +1,13 @@
-#settings go here
-BUZZER = 8
+# Berryclip LED/Buzzer.
 LEDS = [4,17,22,10,9,11]
+BUZZER = 8
+
+# Scale USB Vendor/Product IDs.
+# Dymo M25
+VENDOR_ID = 0x0922
+PRODUCT_ID = 0x8004
+DATA_MODE_GRAMS = 2
+DATA_MODE_OUNCES = 11
 
 #import settings from local_settings.py
 try:
