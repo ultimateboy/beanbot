984ded13eb5800c0088c7a6e385bbe89aa73a5be
Matt Tucker <ultimateboy@gmail.com>
2014-08-30 16:58:28 +0000

    No longer using celery beat to track coffee.

--- beanbot/celeryconfig.py
+++ beanbot/celeryconfig.py
@@ -4,10 +4,3 @@ BROKER_URL = 'redis://localhost:6379/0'
 #BROKER_URL = 'mongodb://localhost:27017/beanbot'
 
 CELERY_TIMEZONE = 'UTC'
-
-CELERYBEAT_SCHEDULE = {
-    'once-a-minute': {
-        'task': 'beanbot.tasks.watch_coffee',
-        'schedule': timedelta(minutes=1),
-    },
-}
